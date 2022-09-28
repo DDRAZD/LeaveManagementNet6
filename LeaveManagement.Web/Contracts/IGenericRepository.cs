29dfd3ffd1ec4def2f67679f0430b48b1a06e0de
Yaniv Nathan <yanivnathan@gmail.com>
2022-09-28 06:59:23 -0400

    added repository

---
+++ LeaveManagement.Web/Contracts/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace LeaveManagement.Web.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+
+        Task<bool> Exists(int id);
+        Task DeleteAsync(int id);
+
+        Task UpdateAsync(T entity);
+        
+    }
+}
---
+++ LeaveManagement.Web/Repositories/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using LeaveManagement.Web.Contracts;
+using LeaveManagement.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Web.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext context;
+        //constructor
+        public GenericRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+            
+
+
+        public async Task<T> AddAsync(T entity)
+        {            
+            
+               await context.AddAsync(entity);
+               await context.SaveChangesAsync();
+               return entity; //we are just returning back the data we added, might be a situation this is needed; entity will actually have a
+                //db id post SaveChanges; i.e. the entity ID will be updated and this is an efficeint way to return the id
+            
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetAsync(id);//using the method implemented below          
+            context.Set<T>().Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<bool> Exists(int id)
+        {
+            var entity = await GetAsync(id); //using the method implemented below
+            return entity != null;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            //context.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.Update(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
