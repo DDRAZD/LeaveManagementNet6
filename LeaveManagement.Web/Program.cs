9ab9af94bcc78156ee80556441be608ab7207b30
Yaniv Nathan <yanivnathan@gmail.com>
2022-11-03 16:30:45 -0400

    added the registration for dependency injection for the leaverequest repository

--- LeaveManagement.Web/Program.cs
+++ LeaveManagement.Web/Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddTransient<IEmailSender>(s=> new EmailSenderMe("localhost", 2
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
 builder.Services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+builder.Services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
 builder.Services.AddAutoMapper(typeof(MapperConfig));
 
 
