35b4d226562489035e7674e7dbe69b84f71e5db8
Yaniv Nathan <yanivnathan@gmail.com>
2022-11-04 16:22:07 -0400

    added out of the box leaverequest controller with entity framework

---
+++ LeaveManagement.Web/Controllers/LeaveRequestsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LeaveManagement.Web.Data;
+
+namespace LeaveManagement.Web.Controllers
+{
+    public class LeaveRequestsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LeaveRequestsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: LeaveRequests
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.LeaveRequests.Include(l => l.LeaveType);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: LeaveRequests/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.LeaveRequests == null)
+            {
+                return NotFound();
+            }
+
+            var leaveRequest = await _context.LeaveRequests
+                .Include(l => l.LeaveType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (leaveRequest == null)
+            {
+                return NotFound();
+            }
+
+            return View(leaveRequest);
+        }
+
+        // GET: LeaveRequests/Create
+        public IActionResult Create()
+        {
+            ViewData["LeaveTypeId"] = new SelectList(_context.LeaveTypes, "Id", "Id");
+            return View();
+        }
+
+        // POST: LeaveRequests/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StartDate,EndDate,LeaveTypeId,DateRequested,RequestComments,RequestingEmployeeId,Approved,Cancelled,Id,DateCreated,DateModified")] LeaveRequest leaveRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(leaveRequest);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LeaveTypeId"] = new SelectList(_context.LeaveTypes, "Id", "Id", leaveRequest.LeaveTypeId);
+            return View(leaveRequest);
+        }
+
+        // GET: LeaveRequests/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.LeaveRequests == null)
+            {
+                return NotFound();
+            }
+
+            var leaveRequest = await _context.LeaveRequests.FindAsync(id);
+            if (leaveRequest == null)
+            {
+                return NotFound();
+            }
+            ViewData["LeaveTypeId"] = new SelectList(_context.LeaveTypes, "Id", "Id", leaveRequest.LeaveTypeId);
+            return View(leaveRequest);
+        }
+
+        // POST: LeaveRequests/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("StartDate,EndDate,LeaveTypeId,DateRequested,RequestComments,RequestingEmployeeId,Approved,Cancelled,Id,DateCreated,DateModified")] LeaveRequest leaveRequest)
+        {
+            if (id != leaveRequest.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(leaveRequest);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LeaveRequestExists(leaveRequest.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LeaveTypeId"] = new SelectList(_context.LeaveTypes, "Id", "Id", leaveRequest.LeaveTypeId);
+            return View(leaveRequest);
+        }
+
+        // GET: LeaveRequests/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.LeaveRequests == null)
+            {
+                return NotFound();
+            }
+
+            var leaveRequest = await _context.LeaveRequests
+                .Include(l => l.LeaveType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (leaveRequest == null)
+            {
+                return NotFound();
+            }
+
+            return View(leaveRequest);
+        }
+
+        // POST: LeaveRequests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.LeaveRequests == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.LeaveRequests'  is null.");
+            }
+            var leaveRequest = await _context.LeaveRequests.FindAsync(id);
+            if (leaveRequest != null)
+            {
+                _context.LeaveRequests.Remove(leaveRequest);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LeaveRequestExists(int id)
+        {
+          return _context.LeaveRequests.Any(e => e.Id == id);
+        }
+    }
+}
