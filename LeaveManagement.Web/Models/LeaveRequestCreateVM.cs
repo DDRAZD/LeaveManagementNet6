8b95dbbcb7b3ad919cfa5bb9d6a7f8d562191a16
Yaniv Nathan <yanivnathan@gmail.com>
2022-11-09 06:41:45 -0500

    added JQuery UI datepicker for better date formats in create page

--- LeaveManagement.Web/Models/LeaveRequestCreateVM.cs
+++ LeaveManagement.Web/Models/LeaveRequestCreateVM.cs
@@ -6,9 +6,9 @@ namespace LeaveManagement.Web.Models
     public class LeaveRequestCreateVM
     {
         [Display(Name ="Leave Start Date")]
-        public DateTime StartDate { get; set; }
+        public DateTime? StartDate { get; set; }
         [Display(Name = "Leave End Date")]
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
 
         [Display(Name ="Leave Types")]
         public int LeaveTypeId { get; set; } //useful for dropdown list
